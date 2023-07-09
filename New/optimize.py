2a291ccb6ee00db982be75c912f174adeb03c4d4
aleukh <aloysiusyap1999@gmail.com>
2023-07-09 21:22:14 +0800

    replaced optimize.py

--- New/optimize.py
+++ New/optimize.py
@@ -34,41 +34,72 @@ class Stack:
 		node = self.head
 
 		while (node != None):
-			#print(node.value, end="")
+			print(node.value, end="")
 			node = node.next
 			if (node != None):
 				print("->", end="")
 		return
 
+def nodes_connected(graph, u, v):
+	return u in graph.neighbors(v)
 
 
-def traversal(graph, start_node):
-	stack = [start_node]
+def traversal(graph, start_node, end_node):
+	stack = Stack()
+	stack.push(start_node)
 	visited = set([start_node])
-	shortest_path = []
-	path = [start_node]
+	route_list = []
+	#print(graph[start_node])
+	#print(f"Start Node: {start_node}\n")
+	j = 0
+	weightage = []
+	myDict = {}
+	
+	while stack.size > 0:
+		start_node = stack.pop()
+		weightage.append(0)
+		#print("This is popped: " + str(start_node) + "\n")
+		#print(graph[start_node])
+		try:
+			neighbours = list(graph[start_node].keys())
+			neighbours = list(neighbors(graph, start_node))
+			print("neighbours: " + neighbours + "\n")
+		except:
+			break
+		#print(neighbours)
+		for neighbour in neighbours:
+			if neighbour not in visited:
+				#print(graph[neighbour])
+				stack.push(neighbour)
+				visited.add(neighbour)
+				myDict[neighbour] = start_node
 
-	while stack:
-		current_node = stack.pop()
+		iter_neighbour = iter(neighbours)
+		for i in iter_neighbour:
+			try:
+				next_node = next(iter_neighbour)
+				#weightage[j] += graph[start_node][next_node][0]['travel_time']
+				#route_list.append(start_node)
+			except StopIteration:
+				break
 
-        # Check if a shorter path to the current node is found
-		if not shortest_path or len(path) < len(shortest_path):
-			shortest_path = list(path)
+			if nodes_connected(graph, start_node, next_node) == True:
+				route_list.append(next_node)
 
-		for neighbor in graph[current_node]:
-			if neighbor not in visited:
-				stack.append(neighbor)
-				visited.add(neighbor)
-
-                # Add the neighbor to the path
-				path.append(neighbor)
-
-	return shortest_path
-
-def find_shortest_path(graph, start_node):
-    return traversal(graph, start_node)
-
-
-
-
-
+			#route_list.append(start_node)
+			weightage[j] += graph[start_node][next_node][0]['travel_time']	
+			'''
+			if graph[start_node][i][0]['length'] <= graph[start_node][next_node][0]['length']:
+				#this block if 1st edge is shorter than 2nd edge to 2nd node
+				#print(str(graph[start_node][i][0]['length']) + " - " + str(graph[start_node][next_node][0]['length']))
+				route_list.append(graph[start_node][i])
+				#print("this is route list" + str(route_list[0]))
+			'''
+		if start_node == end_node:
+			print("THIS IS THE END: " + str(start_node))
+			print("\nTOTAL TRAVEL TIME: " + str(weightage[j]))
+			j += 1
+			print(myDict[start_node])
+			return route_list
+				#weightage.append()
+				#break
