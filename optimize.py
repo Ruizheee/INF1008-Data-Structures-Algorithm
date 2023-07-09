f4e387b7d63bba4a0ca752847880d7c58dfa1099
aleukh <aloysiusyap1999@gmail.com>
2023-07-09 18:39:36 +0800

    testing traversal

--- optimize.py
+++ optimize.py
@@ -42,23 +42,26 @@ class Stack:
 
 
 
-def traversal(graph, start_node):
+def traversal(graph, start_node, end_node):
 	stack = Stack()
 	stack.push(start_node)
 	visited = set([start_node])
 	route_list = []
 	#print(graph[start_node])
 	#print(f"Start Node: {start_node}\n")
+	j = 0
+	weightage = []
 	
 	while stack.size > 0:
 		start_node = stack.pop()
-		print("This is popped: " + str(start_node) + "\n")
+		weightage.append(0)
+		#print("This is popped: " + str(start_node) + "\n")
 		#print(graph[start_node])
 		try:
 			neighbours = list(graph[start_node].keys())
 		except:
 			break
-		print(neighbours)
+		#print(neighbours)
 		for neighbour in neighbours:
 			if neighbour not in visited:
 				#print(graph[neighbour])
@@ -69,13 +72,24 @@ def traversal(graph, start_node):
 		for i in iter_neighbour:
 			try:
 				next_node = next(iter_neighbour)
+				weightage[j] += graph[start_node][next_node][0]['travel_time']
+				route_list.append(start_node)
 			except StopIteration:
 				break
+			'''
 			if graph[start_node][i][0]['length'] <= graph[start_node][next_node][0]['length']:
 				#this block if 1st edge is shorter than 2nd edge to 2nd node
 				#print(str(graph[start_node][i][0]['length']) + " - " + str(graph[start_node][next_node][0]['length']))
 				route_list.append(graph[start_node][i])
 				#print("this is route list" + str(route_list[0]))
+			'''
+			if start_node == end_node:
+				print("THIS IS THE END: " + str(start_node))
+				print("\nTOTAL TRAVEL TIME: " + str(weightage[j]))
+				j += 1
+				return route_list
+				#weightage.append()
+				#break
 
 
 
