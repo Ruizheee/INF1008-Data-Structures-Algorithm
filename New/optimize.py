88e8b09fb59ce514cce6d496f9bb294ff03f5428
aleukh <aloysiusyap1999@gmail.com>
2023-07-10 18:00:44 +0800

    change to bfs and use bfs function!!

--- New/optimize.py
+++ New/optimize.py
@@ -31,7 +31,7 @@ class Queue:
 		if self.front == None:
 			self.rear = None
 
-		return temp
+		return temp.value
 
 def nodes_connected(graph, u, v):
 	return u in graph.neighbors(v)
@@ -39,12 +39,44 @@ def nodes_connected(graph, u, v):
 def bfs_traverse(graph, start_node, end_node):
 	queue = Queue()
 	queue.enqueue(start_node)
+	path = []
 	while queue:
-		path = queue.dequeue()
-		node = list(graph.neighbors(path))[-1]
+		#path.append(current_node)
+		#path = queue.dequeue()
+		current_node = queue.dequeue()
+		path.append(current_node)
+		#path = queue.dequeue()
+		print("path: " + str(path) + "\n")
+		node = path[-1]
 		if node == end_node:
 			return path
 		for neighbours in graph.neighbors(node):
 			new_path = list(path)
+			#new_path = [path]
 			new_path.append(neighbours)
-			queue.append(new_path)
+			for i in new_path:
+				queue.enqueue(i)
+			#queue.enqueue(neighbours)
+
+def backtrace(parent, start_node, end_node):
+	path = [end_node]
+	while path[-1] != start_node:
+		path.append(parent[path[-1]])
+	path.reverse()
+	return path
+
+def bfs(graph, start_node, end_node):
+	parent = {}
+	queue_list = [start_node]
+	queue = Queue()
+	queue.enqueue(start_node)
+	while queue:
+		node = queue.dequeue()
+		print("node: " + str(node) + "\n")
+		if node == end_node:
+			return backtrace(parent, start_node, end_node)
+		for neighbours in graph.neighbors(node):
+			if neighbours not in queue_list:
+				parent[neighbours] = node
+				queue_list.append(neighbours)
+				queue.enqueue(neighbours)
