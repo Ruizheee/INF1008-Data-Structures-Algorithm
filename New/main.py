2517cf9d809005d7a275f135211c539b74b2f750
hellolololol <122359071+hellolololol@users.noreply.github.com>
2023-07-17 03:47:30 +0800

    Update main.py

--- New/main.py
+++ New/main.py
@@ -12,8 +12,9 @@ class MyApp:
                     style={'background-color': '#f0f0f0', 'padding': '20px', 'border-radius': '10px'},
                     children=[
                         html.Div(
-                            [
-                                html.Label('Select Algorithm'),
+                            style={'display': 'flex', 'alignItems': 'center', 'marginBottom': '10px'},  # Set flex display, alignment, and bottom margin
+                            children=[
+                                html.Label('Select Algorithm', style={'marginRight': '10px'}),  # Add margin-right for spacing
                                 dcc.Dropdown(
                                     id='user-dropdown',
                                     options=[
@@ -27,68 +28,47 @@ class MyApp:
                                         'width': '150px',
                                         'display': 'inline-block',
                                         'verticalAlign': 'top',
-                                        'marginLeft': '10px',
-                                        'position': 'relative',  # Add relative positioning
                                     }
                                 ),
-                                html.Div(
-                                    className='dropdown-arrow',  # Add class for styling
-                                    style={
-                                        'position': 'absolute',
-                                        'right': '10px',
-                                        'top': '50%',
-                                        'transform': 'translateY(-50%)',
-                                        'width': '0',
-                                        'height': '0',
-                                        'borderLeft': '6px solid transparent',
-                                        'borderRight': '6px solid transparent',
-                                        'borderTop': '6px solid #999',
-                                        'cursor': 'pointer',  # Add cursor style for clickability
-                                    },
-                                    children=[
-                                        html.A(
-                                            '',  # Empty content, can be replaced with an icon or text
-                                            href='#',  # Set the href attribute to a valid link or '#' for no action
-                                            style={'display': 'block', 'height': '100%', 'width': '100%'},  # Adjust size to match parent div
-                                        )
-                                    ]
-                                )
-                            ],
-                            style={'position': 'relative'}  # Set relative positioning for the container div
+                            ]
                         ),
                         html.Div(id='input-container', style={'margin': '20px'}),
                         html.Div(id='dropdown-value', style={'display': 'none'}),
-                        html.Button(
-                            'Add location',
-                            id='add-location-button',
-                            n_clicks=0,
-                            style={
-                                'background-color': '#10559A',
-                                'color': 'white',
-                                'padding': '10px 20px',
-                                'border': 'none',
-                                'cursor': 'pointer',
-                                'margin-top': '10px',
-                                'margin-right': '10px',  # Add right margin for spacing
-                                'margin-bottom': '10px',
-                                'border-radius': '10px',
-                            }
-                        ),
-                        html.Button(
-                            'Submit',
-                            id='submit-button',
-                            n_clicks=0,
-                            style={
-                                'background-color': '#DB4C77',
-                                'color': 'white',
-                                'padding': '10px 20px',
-                                'border': 'none',
-                                'cursor': 'pointer',
-                                'margin-top': '10px',
-                                'border-radius': '10px',
-                            }
+                        html.Div(
+                            style={'display': 'flex', 'alignItems': 'center', 'justifyContent': 'center', 'marginBottom': '10px'},  # Set flex display, alignment, justify content, and bottom margin
+                            children=[
+                                html.Button(
+                                    'Add location',
+                                    id='add-location-button',
+                                    n_clicks=0,
+                                    style={
+                                        'background-color': '#10559A',
+                                        'color': 'white',
+                                        'padding': '10px 20px',
+                                        'border': 'none',
+                                        'cursor': 'pointer',
+                                        'margin-right': '10px',  # Add right margin for spacing
+                                        'border-radius': '10px',
+                                    }
+                                ),
+                                html.Button(
+                                    'Submit',
+                                    id='submit-button',
+                                    n_clicks=0,
+                                    style={
+                                        'background-color': '#DB4C77',
+                                        'color': 'white',
+                                        'padding': '10px 20px',
+                                        'border': 'none',
+                                        'cursor': 'pointer',
+                                        'border-radius': '10px',
+                                    }
+                                ),
+                            ]
                         ),
                         html.Div(id='output-container', style={'margin-top': '20px'}),
+                        html.Div(id='weight-dropdown-container'),
+                        dcc.Store(id='weight-values-store')
                     ]
                 )
             ]
@@ -101,41 +81,63 @@ class MyApp:
         )(self.add_input_callback)
 
         self.app.callback(
+            Output('weight-dropdown-container', 'children'),
+            [Input('user-dropdown', 'value')]
+        )(self.add_weight_dropdown)
+
+        self.app.callback(
+            Output('weight-values-store', 'data'),
+            [Input({'type': 'weight-dropdown', 'index': ALL}, 'value')],
+            [State('user-dropdown', 'value')]
+        )(self.update_weight_values)
+
+        self.app.callback(
             Output('output-container', 'children'),
             [Input('submit-button', 'n_clicks')],
-            [State('input-container', 'children')]
+            [State('input-container', 'children'), State('user-dropdown', 'value'), State('weight-values-store', 'data')]
         )(self.submit_input_callback)
 
     def add_input_callback(self, n_clicks, children):
-        if n_clicks is not None:
-            inputs = [
-                dcc.Input(
-                    id='user-input',
-                    type='text',
-                    value='',
-                    placeholder='Enter location here',
-                    style={'height': '20px', 'padding': '5px', 'width': '200px', 'border-radius': '3px'}
-                ),
-            ]
+        inputs = children if children is not None else []
+        inputs.append(dcc.Input(id={'type': 'dynamic-input', 'index': n_clicks}, type='text', value='',
+                                placeholder='Enter location here', style={'height': '20px'}))
+        return inputs
 
-            for i in range(n_clicks):
-                inputs.append(html.Br())
-                inputs.append(dcc.Input(
-                    id={'type': 'dynamic-input', 'index': i},
-                    type='text',
-                    value='',
-                    placeholder='Enter location here',
-                    style={'height': '20px', 'padding': '5px', 'width': '200px', 'border-radius': '3px'}
-                ))
+    def add_weight_dropdown(self, dropdown_value):
+        if dropdown_value == 'dijkstra':
+            weight_dropdown = html.Div(
+                children=[
+                    html.Label('Select Factor', style={'marginRight': '10px'}),  # Add label for "Factor" with right margin
+                    dcc.Dropdown(
+                        id={'type': 'weight-dropdown', 'index': 0},
+                        options=[
+                            {'label': 'Length of Road', 'value': 'option 1'},
+                            {'label': 'Speed Limit of Road', 'value': 'option 2'},
+                            {'label': 'Travel Time', 'value': 'option 3'},
+                        ],
+                        value='option 1',
+                        style={'width': '200px'}  # Adjust the width of the dropdown box
+                    ),
+                ],
+                style={'display': 'flex', 'alignItems': 'center', 'marginLeft': '10px'}  # Adjust display and alignment
+            )
+            return weight_dropdown
+        else:
+            return html.Div(id='weight-dropdown-container')
 
-            return inputs
+
+
+    def update_weight_values(self, weight_values, dropdown_value):
+        if dropdown_value == 'dijkstra':
+            return weight_values
         else:
             return []
 
-    def submit_input_callback(self, n_clicks, input_box):
+    def submit_input_callback(self, n_clicks, input_box, dropdown_value, weight_values):
         if input_box is not None:
+            weight_values = weight_values[0]
             map_creator = MapCreator('hotel.csv')
-            return map_creator.submit_inputs(n_clicks, input_box)
+            return map_creator.submit_inputs(n_clicks, input_box, dropdown_value, weight_values)
 
     def run(self):
         self.app.run(debug=True)
